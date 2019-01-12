0a2658f4ab4dcee751d49c14a9acbf9083ffaa6b
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-01-11 19:02:45 -0500

    Create A.cpp

---
+++ Codeforces/EDU 58/A.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+// Head
+
+LL q, l, r, d;
+
+int main() {
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> l >> r >> d;
+        if (d >= l && d <= r) {
+            cout << (r / d + 1) * d << '\n';
+
+        } else cout << d << '\n';
+    }
+    return 0;
+}
