d5c0757cf93d914755aa8c96d99ffc50d57e8124
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-01-28 19:53:54 -0500

    Create DimmaDP.cpp

---
+++ Purdue/Jan 24/DimmaDP.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+// Head
+
+const int N = 3123456;
+const int INF = int(1e9);
+
+int n, a[N], dp[N];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    for (int i = n; i >= 1; i--) {
+        if (i + a[i] >= n) { dp[i] = 1; continue; }
+        dp[i] = INF;
+        for (int j = i + 1; j <= i + a[i]; j++) dp[i] = min(dp[i], dp[j] + 1);
+    }
+    if (dp[1] >= INF) cout << -1 << '\n';
+    else cout << dp[1] << '\n';
+    return 0;
+}
