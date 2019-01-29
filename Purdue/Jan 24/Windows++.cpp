118a112e3056bfdb1b61cf5d1774b3955b80816f
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-01-28 19:50:56 -0500

    Largest Continuous Subsequences

---
+++ Purdue/Jan 24/Windows++.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+// Head
+
+const int N = 1123456;
+const LL INF = LL(1e15);
+
+LL f[N], g[N], a[N];
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) {
+        f[i] = max(a[i], a[i] + f[i - 1]);
+    }
+    for (int i = n; i >= 1; i--) {
+        g[i] = max(a[i], a[i] + g[i + 1]);
+    }
+
+    LL maxm = f[1], ans = -INF;
+    for (int i = 2; i <= n; i++) {
+        ans = max(ans, maxm + g[i]);
+        maxm = max(maxm, f[i]);
+    }
+    cout << ans << '\n';
+    return 0;
+}
