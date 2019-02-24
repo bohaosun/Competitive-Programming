0a58238266cc3e6164cfed2e74a4adbcfdcd21f5
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-02-24 15:04:38 -0500

    Create C.cpp

---
+++ Codeforces/539/C.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+// Head
+
+const int N = 1123456;
+
+int a[N], n;
+int cnt[N][2];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    int cur = 0;
+    LL ans = 0;
+
+    cnt[0][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        int id = (i & 1);
+        cur ^= a[i];
+        //cout << cur << endl;
+        ans += cnt[cur][id];
+        cnt[cur][id]++;
+    }
+    cout << ans << '\n';
+    return 0;
+}
