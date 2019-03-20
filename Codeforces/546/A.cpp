8c7c232587f8e694ffd923ddf184453e31c0712b
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-03-20 18:37:49 -0400

    Create A.cpp

---
+++ Codeforces/546/A.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+
+const int dr[] = { -1, 0, 1, 0, -1, -1, 1, 1 };
+const int dc[] = { 0, -1, 0, 1, -1, 1, -1, 1 };
+// Head
+
+const int N = 112;
+
+int n, l[N], r[N];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> l[i] >> r[i];
+    int x;
+    cin >> x;
+    for (int i = 1; i <= n; i++) {
+        if (x >= l[i] && x <= r[i]) {
+            cout << n - i + 1 << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
