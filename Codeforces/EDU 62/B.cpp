05e99bbd7aaeee9de04b9e5264c7b4ef15d35ce6
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-03-27 18:19:15 -0400

    Create B.cpp

---
+++ Codeforces/EDU 62/B.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+
+const int dr[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dc[] = {0, -1, 0, 1, -1, 1, -1, 1};
+// Head
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        string s;
+        cin >> n >> s;
+        int ans = 123;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '>') { ans = i; break; }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (s[n - i - 1] == '<') { ans = min(ans, i); break; }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
