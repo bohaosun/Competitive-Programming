92be8a3ed2cffae78db1b6678726fb2aa716b17c
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-03-28 17:25:46 -0400

    Create Who let the cows out?.cpp

---
+++ Purdue/Mar 21/Who let the cows out?.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair <LL, LL> pll;
+
+const int dr[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dc[] = {0, -1, 0, 1, -1, 1, -1, 1};
+// Head
+
+const double pi = acos(-1.0);
+
+int n, s, k, m;
+
+int main() {
+    cin >> n >> s >> k >> m;
+    double area = (double)s * s / tan(pi / n) * n / 4;
+
+    double rad = pi - 2 * pi / n;
+    double area2 = rad * k / (2 * pi) * pi * m * m;
+    
+    cout << fixed;
+    cout.precision(2);
+    cout << area - area2 << '\n';
+    return 0;
+}
