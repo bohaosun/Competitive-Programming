88be2ef7d44913d17cb1134deb11114801099a26
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-03-28 17:27:36 -0400

    exGCD

---
+++ Purdue/Mar 21/Intro to Extended Euclidean Algorithm.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end());
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+
+const int dr[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dc[] = {0, -1, 0, 1, -1, 1, -1, 1};
+// Head
+
+void gcd(LL a, LL b, LL& d, LL& x, LL& y) {
+    if(!b) { d = a; x = 1; y = 0; }
+    else { gcd(b, a % b, d, y, x); y -= x * (a / b); }
+}
+
+int m, n;
+
+int main() {
+    cin >> m >> n;
+    LL x, y, d;
+    gcd(m, n, d, x, y);
+    cout << x << ' ' << y << '\n';
+    return 0;
+}
