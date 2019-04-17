8626303ed6519f02ad9c0a07fed873f6f3a5824e
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-04-16 21:57:24 -0400

    Create Implementation.cpp

---
+++ CP-Algorithms/String Processing/Z-function/Implementation.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+
+const int dr[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dc[] = {0, -1, 0, 1, -1, 1, -1, 1};
+// Head
+
+void z_function(string &s, vector<int> &z) {
+    int n = s.length();
+    z.resize(n);  // s[0 ... z[i] - 1] == s[i ... i + z[i] - 1]
+
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
+        if (i <= r) z[i] = min(z[i - l], r - i + 1);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+        if (i + z[i] - 1 > r) { l = i; r = i + z[i] - 1; }
+    }
+}
