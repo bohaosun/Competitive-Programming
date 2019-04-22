83ac7ddd785e45bc77fcdb2d40b945abffb7610e
Bohao Sun <37234843+ZerOoOKnowledge@users.noreply.github.com>
2019-04-22 19:36:37 -0400

    DP & Greedy

---
+++ Codeforces/552/F.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <algorithm>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+
+#define _1 first
+#define _2 second
+#define pb push_back
+#define mp make_pair
+#define sqr(x) ((x) * (x))
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define reset(x, y) memset(x, y, sizeof(x))
+#define BUG(x) cerr << #x << " = " << (x) << endl
+#define BUGP(x) cout << #x << " = " << (x)._1 << ", " << (x)._2 << endl
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef pair<LL, LL> pll;
+
+const int dr[] = { -1, 0, 1, 0, -1, -1, 1, 1 };
+const int dc[] = { 0, -1, 0, 1, -1, 1, -1, 1 };
+// Head
+
+const int N = 212345;
+const int INF = 0x3f3f3f3f;
+
+int n, m, k, a[N], f[N], sum[N], dp[N];
+
+int cost(int l, int r) {
+    int num = r - l + 1;
+    return sum[r] - sum[l - 1 + f[num]];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    sort(a + 1, a + n + 1);
+
+    for (int i = 1; i <= k; i++) sum[i] = sum[i - 1] + a[i];
+
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        f[x] = max(f[x], y);
+    }
+
+    for (int i = 1; i <= k; i++) {
+        dp[i] = INF;
+        for (int j = 1; j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j] + cost(i - j + 1, i));
+        }
+    }
+
+    cout << dp[k] << '\n';
+    return 0;
+}
